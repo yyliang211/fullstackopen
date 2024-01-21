b1ecc8b21b3f4107888e709d6c85d2b471f9f7cf
Yingyang Liang <yingyang.liang@tufts.edu>
2024-01-21 14:38:42 -0500

    finished 1b

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,24 +1,27 @@
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header header={course} />
-      <Content
-        part1={part1}
-        part2={part2}
-        part3={part3}
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
+      <Content parts={parts} />
+      <Total
+        totalCount={parts.reduce((acc, currVal) => acc + currVal.exercises, 0)}
       />
-      <Total totalCount={exercises1 + exercises2 + exercises3} />
     </div>
   );
 };
@@ -27,21 +30,21 @@ function Header({ header }) {
   return <h1>{header}</h1>;
 }
 
-function Content({ part1, part2, part3, exercises1, exercises2, exercises3 }) {
+function Part({ part }) {
   return (
-    <div>
-      <Part part={part1} exercise={exercises1} />
-      <Part part={part2} exercise={exercises2} />
-      <Part part={part3} exercise={exercises3} />
-    </div>
+    <p>
+      {part.name} {part.exercises}
+    </p>
   );
 }
 
-function Part({ part, exercise }) {
+function Content({ parts }) {
   return (
-    <p>
-      {part} {exercise}
-    </p>
+    <div>
+      {parts.map((part) => (
+        <Part part={part} />
+      ))}
+    </div>
   );
 }
 
