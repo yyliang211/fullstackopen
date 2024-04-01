56f3e6c87ce74e70c9730b5ef25907143aa092f5
Yingyang Liang <yingyang.sas@gmail.com>
2024-03-31 22:03:27 -0400

    add scripts for build and deploy

--- part3/index.js
+++ part3/index.js
@@ -31,6 +31,7 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
+app.use(express.static("dist"));
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
