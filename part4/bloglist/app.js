b183ca1e1f1bed7593b32d97e97acb6eb7988a9e
Yingyang Liang <yingyang.liang@tufts.edu>
2024-04-14 20:52:02 -0400

    done with phonebook reorg

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -2,15 +2,11 @@ const config = require("./utils/config");
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const notesRouter = require("./controllers/notes");
-const middleware = require("./utils/middleware");
+const blogRouter = require("./controllers/blogs");
 const logger = require("./utils/logger");
 const mongoose = require("mongoose");
 
-mongoose.set("strictQuery", false);
-
-logger.info("connecting to", config.MONGODB_URI);
-
+const mongoUrl = "mongodb://localhost/bloglist";
 mongoose
   .connect(config.MONGODB_URI)
   .then(() => {
@@ -21,12 +17,8 @@ mongoose
   });
 
 app.use(cors());
-app.use(express.static("dist"));
 app.use(express.json());
-app.use(middleware.requestLogger);
 
-app.use("/api/notes", notesRouter);
-app.use(middleware.unknownEndpoint);
-app.use(middleware.errorHandler);
+app.use("/api/blogs", blogRouter);
 
 module.exports = app;
