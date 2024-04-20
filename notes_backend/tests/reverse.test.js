7c985831b5f010769f4580627a35e1781f27edca
Yingyang Liang <yingyang.liang@tufts.edu>
2024-04-20 17:30:46 -0400

    use jest

--- notes_backend/tests/reverse.test.js
+++ notes_backend/tests/reverse.test.js
@@ -1,22 +1,19 @@
-const { test } = require("node:test");
-const assert = require("node:assert");
-
 const reverse = require("../utils/for_testing").reverse;
 
 test("reverse of a", () => {
   const result = reverse("a");
 
-  assert.strictEqual(result, "a");
+  expect(result).toBe("a");
 });
 
 test("reverse of react", () => {
   const result = reverse("react");
 
-  assert.strictEqual(result, "tcaer");
+  expect(result).toBe("tcaer");
 });
 
-test("reverse of saippuakauppias", () => {
-  const result = reverse("saippuakauppias");
+test("reverse of releveler", () => {
+  const result = reverse("releveler");
 
-  assert.strictEqual(result, "saippuakauppias");
+  expect(result).toBe("releveler");
 });
