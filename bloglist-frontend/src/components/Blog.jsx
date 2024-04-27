ceef2b7dbf5e5097e23336a1b5286704896cbbe6
Yingyang Liang <yingyang.liang@tufts.edu>
2024-04-27 12:16:48 -0400

    5.7

--- bloglist-frontend/src/components/Blog.jsx
+++ bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,33 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [expanded, setExpanded] = useState(false);
+  const display = expanded ? "" : "none";
+  const buttonLabel = expanded ? "hide" : "view";
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={() => setExpanded(!expanded)}>{buttonLabel}</button>
+      </div>
+      <div style={{ display: display }}>
+        <div>{blog.url}</div>
+        <div>
+          likes {blog.likes} <button>like</button>
+        </div>
+        <div>{blog.user.name}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
